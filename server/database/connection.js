e98b16a4c9133a5cb14bdcf892ac6b38184a8076
kasula vaishnavi <kasulavaishnavi7@gmail.com>
2025-04-17 15:37:38 +0530

    changes commit

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kasulavaishnavi4:Kasula2002@cluster0.dyqj3fj.mongodb.net/NotesApp")
+mongoose.connect("mongodb+srv://kasulavaishnavi4:Kasula2002@cluster0.dyqj3fj.mongodb.net")
 .then(()=>{
     console.log("connection established")
 }).catch((err)=>{
     console.log(`Error is : ${err}`)
 })
-
-// mongodb+srv://kasulavaishnavi4:kasula2002@cluster0.dyqj3fj.mongodb.net/
-// mongodb+srv://kasulavaishnavi4:kasula2002@cluster0.dyqj3fj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// mongodb+srv://saswatabasu:saswata23@cluster0.ng91g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0npx 
